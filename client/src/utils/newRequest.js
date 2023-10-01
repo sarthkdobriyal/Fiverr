9fef132dd2dd37712320cc6c7f877492bfd9b838
sarthkdobriyal <dobriyalsarthaksa.8@gmail.com>
2023-10-02 03:14:23 +0530

    prod3

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const newRequest = axios.create({
-    baseURL: "http://localhost:8800/api/",
+    baseURL: "https://fiverr-kvdkgp8hw-high-prog.vercel.app/api/",
     withCredentials:true,
 })
 
